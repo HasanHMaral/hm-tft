699dd1a7dbb93a5d55c90745cbd49338b8b6b936
HasanHMaral <58922694+HasanHMaral@users.noreply.github.com>
2024-12-26 18:15:54 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,14 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+from darts.timeseries import TimeSeries
+from darts.models import TFTModel
+from darts.explainability import TFTExplainer
 
 st.title('🎈 Hava Kirliliği Tahmini Uygulaması')
 
 st.info('Bu uygulama derin öğrenme modeli ile tahmin yapar!')
+
+# Veri Yükleme
+st.header("Veri Yükleme")
+uploaded_file = st.file_uploader("Hava Kalitesi Verisi Yükleyin (CSV formatında)", type="csv")
