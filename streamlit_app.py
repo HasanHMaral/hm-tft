44a02c12c9bd654d355a0190226b60740031a291
HasanHMaral <58922694+HasanHMaral@users.noreply.github.com>
2024-12-27 00:01:10 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ if uploaded_file is not None:
 
 else:
     st.warning("Lütfen bir CSV dosyası yükleyin.")
-"""# TimeSeries Nesnesi Oluşturma
+'''# TimeSeries Nesnesi Oluşturma
 st.subheader("TimeSeries Nesnesi")
 zaman_serisi = TimeSeries.from_dataframe(data, value_cols="y")
 st.write("TimeSeries Nesnesi Başarıyla Oluşturuldu!")
@@ -178,4 +178,4 @@ if st.button("Modeli Eğit"):
 
     except Exception as e:
         st.error(f"Model oluşturulurken veya eğitilirken bir hata oluştu: {str(e)}")
-"""
+'''
